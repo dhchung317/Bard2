ba4d7808e8520b492e3927d7f169fef53612882f
hyunki <dhchung317@gmail.com>
2019-12-17 18:08:14 -0500

    remove all files

--- app/src/main/java/com/hyunki/bard2/controller/ClickableNoteListener.kt
+++
@@ -1,7 +0,0 @@
-package com.hyunki.bard2.controller
-
-import com.hyunki.bard2.model.ClickableNote
-
-interface ClickableNoteListener {
-    fun setCurrentNote(note: ClickableNote)
-}
--- app/src/main/java/com/hyunki/bard2/model/ClickableNote.kt
+++
@@ -1,3 +0,0 @@
-package com.hyunki.bard2.model
-
-class ClickableNote(val note: String, val rawNote: Int, val imgSrc: Int)
